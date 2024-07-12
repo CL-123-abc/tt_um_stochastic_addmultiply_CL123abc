508de7da89bb2eeb3c97af30329e0defddbcf876
CL-123-abc <lciecen@gmail.com>
2024-07-12 15:03:53 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_CL-123-abc (
+module tt_um_CL(
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
