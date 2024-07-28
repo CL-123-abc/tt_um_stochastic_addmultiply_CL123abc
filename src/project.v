db1ef6352ca88a81ee30ef5642f2c9d2e2403513
CL-123-abc <lciecen@gmail.com>
2024-07-27 21:22:45 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -22,22 +22,61 @@ module tt_um_stochastic_multiplier_CL123abc(
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-    reg [30:0] lfsr; 
+    reg [30:0] lfsr_1, lfsr_2;
+    reg [7:0] input_prob;
+    reg SN_Bit_1, SN_Bit_2, SN_Bit_Out;
+    reg [3:0] clk_counter;
+    reg [2:0] prob_counter;
+    reg [2:0] output_prob;
+    reg overflow;
+     
     always @(posedge clk or posedge rst_n) begin
         if (rst_n) begin
-        lfsr <= 31'd1; // Reset counter
+        lfsr_1 <= 31'd1; // Reset 1st counter
+	    lfsr_2 <= 31'd2; // Reset 2nd counter to different value
+	    SN_Bit_1 <= 1'b0; // Reset SN bits
+	    SN_Bit_2 <= 1'b0; 
+	    SN_Bit_Out <= 1'b0; 
+	    clk_counter <= 4'b0000; // Reset clk counter
+	    output_prob <= 3'b000; // Reset output
+	    overflow <= 0; // Reset overflow
     end else begin
         // Increment counter on each clock cycle
-        lfsr[0] <= lfsr[27] ^ lfsr[30] ;
-        lfsr[30:1] <=lfsr[29:0] ;  
+        lfsr_1[0] <= lfsr_1[27] ^ lfsr_1[30] ;
+        lfsr_1[30:1] <=lfsr_1[29:0] ;
+
+	    lfsr_2[0] <= lfsr_2[27] ^ lfsr_2[30] ;
+        lfsr_2[30:1] <=lfsr_2[29:0] ;
+
+	    // Comparator used to generate Bipolar Stochastic Number from 4-bit probability.
+	    // Compare RN from LFSR with probability wanted in BN and generate 1 when RN < BN
+	    SN_Bit_1 <= (lfsr_1[30:27] < ui_in[3:0]) ;
+	    SN_Bit_2 <= (lfsr_2[30:27] < ui_in[7:4]) ;
+
+	    // Stochastic Multiplier for Bipolar SN uses XNOR gate
+	    SN_Bit_Out <= !(SN_Bit_1 ^ SN_Bit_2) ;
+	
+	    // To convert back to binary probability, use an up-counter, outputting the number of 1s in every 8 bits
+	    if (SN_Bit_Out == 1) begin
+	        if (prob_counter == 3'b111) begin
+		    overflow <= 1; // if the number of bits is 8, overflow
+	        end
+	    prob_counter <= prob_counter + 3'b001;
+	    end 
+        
+	    if (clk_counter == 4'b1000) begin // output only when clk_counter has counted 8 cycles.
+	    output_prob <= prob_counter;
+	    overflow <= 0;
+	    end
+	    clk_counter <= clk_counter + 4'b0001;
     end
 end  
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out[0] =lfsr[30] ;
+  assign uo_out[2:0] = output_prob ;
+  assign uo_out[3] = overflow;
   assign uio_out = 0;
   assign uio_oe  = 0;
-  assign uo_out[7:1]= 7'd0;
+  assign uo_out[7:4] = 3'b000;
   // List all unused inputs to prevent warnings
-    wire _unused = &{ena, uio_in, ui_in, 1'b0}; 
+    wire _unused = &{ena, uio_in, 1'b0}; 
 endmodule
-
