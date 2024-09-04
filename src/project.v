e40e5a019c88ba289405b5b3749eaa14fec6a70b
CL123abc <lciecen@gmail.com>
2024-09-04 14:19:43 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -21,7 +21,7 @@
 
 `default_nettype none
 
-module tt_um_stochastic_addmultiply_CL123abc(
+module test(
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -32,15 +32,15 @@ module tt_um_stochastic_addmultiply_CL123abc(
     input  wire       rst_n     // reset_n - low to reset
 );
 	
-	wire [8:0] input_precheck_1, input_precheck_2, input_postcheck_1;
+	wire [8:0] input_1, input_2;
 	wire [30:0] lfsr;
-	wire SN_Bit_1, SN_Bit_2, SN_Bit_smul_in, SN_Bit_sel;
+	wire SN_Bit_1, SN_Bit_2, SN_Bit_sel;
 	wire SN_Bit_mul_out, SN_Bit_add_out, SN_Bit_smul_out;
     wire [8:0] mul_avg, add_avg, smul_avg;
     wire mul_bit_out, add_bit_out, smul_bit_out;
     reg [17:0] clk_counter;
     
-	/* SUBMODULES USED:
+	/* SUBMODULES:
      *
      * /////////////////////////////////////////////////////////////////////////////
 	 * SUBMODULE NAME:
@@ -62,7 +62,7 @@ module tt_um_stochastic_addmultiply_CL123abc(
      * \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 	 *
      * /////////////////////////////////////////////////////////////////////////////
-	 * SUBMODULE NAME:
+	 * (UNUSED) SUBMODULE NAME:
 	   input_checker (.input_precheck(), .output_postcheck());
      * SUBMODULE DESCRIPTION:
 	 * Checks the input to see if it is within the desired limits. 
@@ -89,8 +89,8 @@ module tt_um_stochastic_addmultiply_CL123abc(
 	 *
 	 * /////////////////////////////////////////////////////////////////////////////
 	 * SUBMODULE NAME:
-  	   SN_Generators (.lfsr(), .Input_1(), .Input_2(), .Input_3(),
-	                  .SN_Bit_1(), .SN_Bit_2(), .SN_Bit_3(), .SN_Bit_sel());
+  	   SN_Generators (.lfsr(), .Input_1(), .Input_2(),
+	                  .SN_Bit_1(), .SN_Bit_2(), .SN_Bit_sel());
      * SUBMODULE DESCRIPTION:
 	 * 
      * INPUTS:
@@ -114,21 +114,19 @@ module tt_um_stochastic_addmultiply_CL123abc(
      */
   
     serial_to_value_input global_input(.clk(clk), .clk_counter(clk_counter), .rst_n(rst_n), 
-									   .input_bit_1(ui_in[0]), .output_bitseq_1(input_precheck_1), 
-									   .input_bit_2(ui_in[1]), .output_bitseq_2(input_precheck_2));
-	
-	input_checker        smul_incheck(.input_precheck(input_precheck_1), .output_postcheck(input_postcheck_1));
+									   .input_bit_1(ui_in[0]), .output_bitseq_1(input_1), 
+									   .input_bit_2(ui_in[1]), .output_bitseq_2(input_2));
 	
 	LFSR                 global_lsfr(.clk(clk), .rst_n(rst_n), .lfsr(lfsr));
 	
 	SN_Generators        global_SN_gen(.lfsr(lfsr), 
-	                                   .Input_1(input_precheck_1), .Input_2(input_precheck_2), .Input_3(input_postcheck_1),
-	                                   .SN_Bit_1(SN_Bit_1), .SN_Bit_2(SN_Bit_2), .SN_Bit_3(SN_Bit_smul_in),
+	                                   .Input_1(input_1), .Input_2(input_2), 
+	                                   .SN_Bit_1(SN_Bit_1), .SN_Bit_2(SN_Bit_2), 
 	                                   .SN_Bit_sel(SN_Bit_sel));
 	
 	multiplier           mul(.SN_Bit_1(SN_Bit_1), .SN_Bit_2(SN_Bit_2), .SN_Bit_Out(SN_Bit_mul_out));
 	adder                add(.SN_Bit_1(SN_Bit_1), .SN_Bit_2(SN_Bit_2), .SN_Bit_sel(SN_Bit_sel), .SN_Bit_Out(SN_Bit_add_out));
-	self_multiplier      smul(.clk(clk), .rst_n(rst_n), .SN_Bit_1(SN_Bit_smul_in), .SN_Bit_Out(SN_Bit_smul_out));
+	self_multiplier      smul(.clk(clk), .SN_Bit_1(SN_Bit_1), .SN_Bit_Out(SN_Bit_smul_out));
 	
 	up_counter           mul_up_counter(.clk(clk), .rst_n(rst_n), .SN_Bit_Out(SN_Bit_mul_out), 
 	                                    .out_set(2'b00), .clk_counter(clk_counter), .average(mul_avg));
@@ -160,20 +158,27 @@ module tt_um_stochastic_addmultiply_CL123abc(
   
   // PIN LAYOUT
   // All output pins must be assigned. If not used, assign to 0.
+  
+  assign uo_out[7:0] = smul_avg[8:1];
+  assign uio_out[0] = smul_avg[0];
+  assign uio_out[7:1] = 7'b0;
+  assign uio_oe[7:0] = 8'b1;
+  
+  /*
   assign uo_out[0] = mul_bit_out;
   assign uo_out[1] = add_bit_out; 
   assign uo_out[2] = smul_bit_out;
   assign uo_out[3] = clk_counter[17];
-  assign uo_out[4] = clk;
-  assign uo_out[7:5] = 3'b0;
+  assign uo_out[7:4] = 4'b0;
   assign uio_out[7:0] = 8'b0;    
-  assign uio_oe[7:0]  = 8'b00000000;
+  assign uio_oe[7:0]  = 8'b0;
+  */
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, ui_in[7:2], uio_in, 1'b0}; 
 endmodule
 
-/* SUBMODULES:
- *
+/* 
+ * SUBMODULES
  */
 
 module serial_to_value_input(clk, clk_counter, rst_n, input_bit_1, output_bitseq_1, input_bit_2, output_bitseq_2);
@@ -235,6 +240,7 @@ reg [4:0] adjustment;
 	end
 endmodule
 
+/* UNUSED
 module input_checker(input_precheck, output_postcheck); 
 input wire [8:0] input_precheck;
 wire over_limit, under_limit, limit;
@@ -244,6 +250,100 @@ assign over_limit = (input_precheck > 9'b100001111);
 assign under_limit = (input_precheck < 9'b011110001);
 assign limit = (over_limit||under_limit) == 1;
 assign output_postcheck = (limit == 1)? ((over_limit == 1)? 9'b100001111 : 9'b011110001) : input_precheck;
+endmodule
+*/
+
+module LFSR(clk, rst_n, lfsr);
+input wire clk, rst_n;
+output reg [30:0] lfsr;
+	always@(posedge clk or posedge rst_n) begin
+        if (rst_n == 1)
+            lfsr <= 31'd1349395;
+        else begin
+            lfsr[0] <= lfsr[27] ^ lfsr[30] ;
+            lfsr[30:1] <= lfsr[29:0] ;
+		end
+    end
+endmodule
+
+module SN_Generators(lfsr, Input_1, Input_2, SN_Bit_1, SN_Bit_2, SN_Bit_sel);
+input wire [30:0] lfsr;
+input wire [8:0] Input_1, Input_2;
+output wire SN_Bit_1, SN_Bit_2, SN_Bit_sel;
+
+assign SN_Bit_1 = (lfsr[8:0] < Input_1[8:0]) ;
+assign SN_Bit_2 = ({lfsr[20:12]} < Input_2[8:0]) ;
+assign SN_Bit_sel = ({lfsr[3:1], lfsr[30:26], lfsr[11]} < 9'b100000000);
+wire unused = &{1'b0, lfsr[25:21], lfsr[10:9], 1'b0};
+endmodule
+
+module multiplier(SN_Bit_1, SN_Bit_2, SN_Bit_Out);
+input wire SN_Bit_1, SN_Bit_2;
+output wire SN_Bit_Out;
+	
+assign SN_Bit_Out = !(SN_Bit_1 ^ SN_Bit_2);
+endmodule
+
+module adder(SN_Bit_1, SN_Bit_2, SN_Bit_sel, SN_Bit_Out);
+input wire SN_Bit_1, SN_Bit_2, SN_Bit_sel;
+output wire SN_Bit_Out;
+	
+assign SN_Bit_Out = (SN_Bit_sel == 0) ? SN_Bit_1 : SN_Bit_2;
+endmodule
+
+module self_multiplier(clk, SN_Bit_1, SN_Bit_Out);
+input wire clk, SN_Bit_1;
+output wire SN_Bit_Out; 
+wire SN_Bit_Q;
+D_FF delay_1_SN_Bit(clk, SN_Bit_1, SN_Bit_Q);
+
+assign SN_Bit_Out = !(SN_Bit_1 ^ SN_Bit_Q);
+endmodule
+
+module D_FF(clk, D, Q);
+input wire clk, D;
+output reg Q;
+	
+	always@(posedge clk)begin
+	       Q <= D;
+	end
+endmodule
+
+module up_counter(clk, rst_n, SN_Bit_Out, out_set, clk_counter, average);
+input wire clk, rst_n, SN_Bit_Out;
+input wire [1:0] out_set;
+input wire [17:0] clk_counter;
+output reg [8:0] average;
+reg [16:0] prob_counter;
+//reg over_flag;
+
+	always@(posedge clk or posedge rst_n) begin
+		if (rst_n) begin
+            average <= 0;
+            prob_counter <= 0;
+            //over_flag <= 0;
+        end 
+    	else begin
+        	if (SN_Bit_Out == 1) begin
+				if (prob_counter == 17'd131071) begin
+		            //over_flag <= 1; 
+		            prob_counter <= 17'b0;
+	            end
+	            else 
+					prob_counter <= prob_counter + 17'b1;
+	        end 
+	        if (clk_counter == 18'd131072) begin 
+				case(out_set)                           //For scaling, if needed.
+			    2'b00: average <= {prob_counter[16:8]}; //Multiplier
+			    2'b01: average <= {prob_counter[16:8]}; //Adder 
+			    2'b10: average <= {prob_counter[16:8]}; //Self-Multiplier
+			    default:;
+	    		endcase
+	    		//over_flag <= 0; 
+	    		prob_counter <= 17'b0; 
+	    	end
+	    end
+	end
 endmodule
 
 module value_to_serial_output(clk, rst_n, input_bits, output_bit);
@@ -277,102 +377,3 @@ output reg output_bit;
          end
     end
 endmodule
-
-module LFSR(clk, rst_n, lfsr);
-input wire clk, rst_n;
-output reg [30:0] lfsr;
-	always@(posedge clk or posedge rst_n) begin
-        if (rst_n == 1)
-            lfsr <= 31'd134995;
-        else begin
-            lfsr[0] <= lfsr[27] ^ lfsr[30] ;
-            lfsr[30:1] <= lfsr[29:0] ;
-		end
-    end
-endmodule
-
-module SN_Generators(lfsr, Input_1, Input_2, Input_3, SN_Bit_1, SN_Bit_2, SN_Bit_3, SN_Bit_sel);
-input wire [30:0] lfsr;
-input wire [8:0] Input_1, Input_2, Input_3;
-output wire SN_Bit_1, SN_Bit_2, SN_Bit_3, SN_Bit_sel;
-
-assign SN_Bit_1 = (lfsr[8:0] < Input_1[8:0]) ;
-assign SN_Bit_2 = ({lfsr[20:12]} < Input_2[8:0]) ;
-assign SN_Bit_3 = (lfsr[8:0] < Input_3[8:0]);
-assign SN_Bit_sel = ({lfsr[3:1], lfsr[30:26], lfsr[11]} < 9'b100000000);
-wire unused = &{1'b0, lfsr[25:21], lfsr[10:9], 1'b0};
-endmodule
-
-module multiplier(SN_Bit_1, SN_Bit_2, SN_Bit_Out);
-input wire SN_Bit_1, SN_Bit_2;
-output wire SN_Bit_Out;
-	
-assign SN_Bit_Out = !(SN_Bit_1 ^ SN_Bit_2);
-endmodule
-
-module adder(SN_Bit_1, SN_Bit_2, SN_Bit_sel, SN_Bit_Out);
-input wire SN_Bit_1, SN_Bit_2, SN_Bit_sel;
-output wire SN_Bit_Out;
-	
-assign SN_Bit_Out = (SN_Bit_sel == 0) ? SN_Bit_1 : SN_Bit_2;
-endmodule
-
-module self_multiplier(clk, rst_n, SN_Bit_1, SN_Bit_Out);
-input wire clk, rst_n, SN_Bit_1;
-output wire SN_Bit_Out; 
-wire SN_Bit_Q;
-	
-D_FF delay_SN_Bit(clk, rst_n, SN_Bit_1, SN_Bit_Q);
-	
-assign SN_Bit_Out = !(SN_Bit_1 ^ SN_Bit_Q);
-endmodule
-
-module D_FF(clk, rst_n, D, Q);
-input wire clk, rst_n, D;
-output reg Q;
-	
-	always@(posedge clk or posedge rst_n)begin
-		if(rst_n)
-			Q <= 0;
-		else
-			Q <= D;
-	end
-endmodule
-
-module up_counter(clk, rst_n, SN_Bit_Out, out_set, clk_counter, average);
-input wire clk, rst_n, SN_Bit_Out;
-input wire [1:0] out_set;
-input wire [17:0] clk_counter;
-output reg [8:0] average;
-reg [16:0] prob_counter;
-//reg over_flag;
-
-	always@(posedge clk or posedge rst_n) begin
-		if (rst_n) begin
-            average <= 0;
-            prob_counter <= 0;
-            //over_flag <= 0;
-        end 
-    	else begin
-        	if (SN_Bit_Out == 1) begin
-				if (prob_counter == 17'd131071) begin
-		            //over_flag <= 1; 
-		            prob_counter <= 17'b0;
-	            end
-	            else 
-					prob_counter <= prob_counter + 17'b1;
-	        end 
-	        if (clk_counter == 18'd131072) begin 
-				case(out_set)
-			    2'b00: average <= {prob_counter[16:8]}; //Multiplier
-			    2'b01: average <= {prob_counter[16:8]}; //Adder ??
-			    2'b10: average <= {prob_counter[8:0]};  // Self-Multiplier
-			    default:;
-	    		endcase
-	    		//over_flag <= 0; 
-	    		prob_counter <= 17'b0; 
-	    	end
-	    end
-	end
-endmodule
-
