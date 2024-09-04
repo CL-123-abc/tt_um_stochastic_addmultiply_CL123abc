74fe6faa9a07747cd88795d0f3d1abb0c5836745
CL123abc <lciecen@gmail.com>
2024-09-04 14:21:12 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -21,7 +21,7 @@
 
 `default_nettype none
 
-module test(
+module tt_um_stochastic_addmultiply_CL123abc(
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
